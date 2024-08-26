d3836be5b26b634dc581edc865ae3dac47014e07
guilherme-kunsch <guilherme.kunsch@outlook.com>
2024-08-26 13:47:15 -0300

    finalizando exercicio 6

--- lampada/lampada.go
+++ lampada/lampada.go
@@ -1,11 +1,16 @@
 package lampada
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
 
 type Lampada struct {
 	Voltagem float64
 	Potencia float64
 	Estado   bool
+	Queimada bool
 }
 
 func (l *Lampada) AlterarVoltPoten(valorPotencia, valorVoltagem float64) {
@@ -24,6 +29,13 @@ func (l *Lampada) LerPotencia() float64 {
 }
 
 func (l *Lampada) Ligado() bool {
+	if rand.Float64() <= 0.15 {
+		l.Queimada = true
+		fmt.Println("A lâmpada queimou ao ser ligada!")
+		l.Estado = false
+		return false
+	}
+
 	l.Estado = true
 	return l.Estado
 }
@@ -38,5 +50,14 @@ func (l *Lampada) ExibirEstado() {
 	if l.Estado {
 		estado = "ligada"
 	}
+	if l.Queimada {
+		estado = "queimada"
+	}
 	fmt.Printf("Lâmpada está %s, Potência: %.2fW, Voltagem: %.2fV\n", estado, l.Potencia, l.Voltagem)
 }
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+//a lampada tem 15% de chances de ser queimada ao ser ligada
